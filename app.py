8b69aea440cfd88956ccb4fa915feecb3c9c7ec0
Pranjal Sinha <pranjal01sinha@gmail.com>
2026-06-03 23:40:44 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from streamlit.web import cli as stcli
+import runpy
 import sys
 
-if __name__ == "__main__":
-    sys.argv = ["streamlit", "run", "src/main.py"]
-    sys.exit(stcli.main())
+sys.argv = ["streamlit", "run", "src/main.py", "--server.port=7860", "--server.address=0.0.0.0"]
+runpy.run_module("streamlit", run_name="__main__")
